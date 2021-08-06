a9ba57868813b03d9352b0da0d4dc61b225528dd
Geng Yuan <mengmiao.0101@icloud.com>
2021-08-06 21:38:23 +0800

    ✨ mysql 引入以及一些请求封装

---
+++ CSGO-Back/utils/query.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql");
+const MYSQL_CONFIG = require("../config/mysql_config.js");
+
+// mysql
+const pool = mysql.createPool(MYSQL_CONFIG);
+
+// query sql 语句接口
+const query = (sql, callback) => {
+	return new Promise((resolve, reject) => {
+		pool.getConnection((err, connection) => {
+			if (err) {
+				return reject(err);
+			}
+			connection.query(sql, (err, rows) => {
+				if (err) {
+					return reject(err);
+				}
+				connection.release();
+				resolve(rows);
+			});
+		});
+	});
+};
