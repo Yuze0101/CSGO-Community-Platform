a7dc2988a21105b1c3b3a17bc816d77ca8b7662c
Geng Yuan <mengmiao.0101@icloud.com>
2021-08-08 12:29:25 +0800

    ✨ 完成一部分功能封装

---
+++ CSGO-Front/src/store/users.js
@@ -0,0 +1,8 @@
+import $api from "@/api";
+export default {
+	namespaced: true,
+	// state: {},
+	// getters: {},
+	// mutations: {},
+	actions: {},
+};
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CSGO-Community-Platform
 CSGO-Community-Platform
+## Install
+```
+cd CSGO-Back && yarn
+cd CSGO-Front && yarn
+```
+## Run
