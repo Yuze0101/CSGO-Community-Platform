a6ff6fec9ebb3cd90a1f67544e5bba928114673a
Geng Yuan <mengmiao.0101@icloud.com>
2021-09-16 14:40:40 +0800

    📖 虽然不知道改了啥 反正push一下

--- CSGO-Front/src/router/index.js
+++ CSGO-Front/src/router/index.js
@@ -1,38 +1,38 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../views/Home.vue";
-import MainPage from "@/components/MainPage.vue";
-const Login = () => import("@/components/Login.vue");
-const PlayPage = () => import("@/components/PlayPage.vue");
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import MainPage from '@/components/MainPage.vue'
+const Login = () => import('@/components/Login.vue')
+const PlayPage = () => import('@/components/PlayPage.vue')
 
 const routes = [
-	{
-		path: "/home",
-		name: "home",
-		component: Home,
-		children: [
-			{
-				path: "main",
-				name: "mainpage",
-				component: MainPage,
-				meta: { index: 0 },
-			},
-			{
-				path: "play",
-				name: "playpage",
-				component: PlayPage,
-				meta: { index: 1 },
-			},
-			{
-				path: "login",
-				name: "login",
-				component: Login,
-				meta: { index: 2 },
-			}
-		],
-	},
-];
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+    children: [
+      {
+        path: 'main',
+        name: 'mainpage',
+        component: MainPage,
+        meta: { index: 0 },
+      },
+      {
+        path: 'play',
+        name: 'playpage',
+        component: PlayPage,
+        meta: { index: 1 },
+      },
+      {
+        path: 'login',
+        name: 'login',
+        component: Login,
+        meta: { index: 2 },
+      },
+    ],
+  },
+]
 
 export default createRouter({
-	history: createWebHashHistory(),
-	routes: routes,
-});
+  history: createWebHashHistory(),
+  routes: routes,
+})
